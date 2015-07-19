9328f6542c4de53bd69d7fe20a7d84baf2796c75
kevin <kevinli7@gmail.com>
2015-07-18 20:51:38 -0700

    added more to html

--- app/views.py
+++ app/views.py
@@ -5,7 +5,18 @@ from app import app
 @app.route('/index')
 def index():
     user = {'nickname' : "DOGE"}
+    posts = [
+        {
+            'author': {'nickname': "DogeLuvr69"},
+	    'body': 'So many DOGES in the house!!'
+	},
+	{
+	    'author': {'nickname': 'Doge Enthusiast',
+	    'body': 'WHO LET THE DOGES OUT'
+	}
+    ]
     return render_template('index.html',
                             title='DOGELAND', 
-			    user=user)
+			    user=user,
+			    posts=posts)
     
